11c1485cf7bc350d3427341a743e243a55c8b432
haeun <gkdms1120s@gmail.com>
2021-12-16 17:32:06 +0900

    1216 11-A error resolve

--- week11/Week11_A_12191586_HaeunKim.cpp
+++ week11/Week11_A_12191586_HaeunKim.cpp
@@ -1,26 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
 using namespace std;
 
-int cards[27] = { 0 };
+int score[24];
+bool t_f[8388610];
+int needAlphabet[24][27];
+int restAlphabet[8388609][27];
+int m, answer;
+string word;
+int getScore[8388609];
+
+void solution() {
+
+	getScore[0] = 0;
+
+	for (int i = 1; i <= m; i++) {
+		int status = 1 << (i - 1);
+
+		for (int j = 0; j < status; j++) {
+			bool flag = false;
+
+			for (int k = 1; k <= 26; k++) {
+				if (restAlphabet[j][k] < needAlphabet[i][k]) {
+					flag = true;
+				}
+			}
+
+			if (flag == true) continue;
+
+			for (int k = 1; k <= 26; k++) {
+				restAlphabet[j + status][k] = restAlphabet[j][k] - needAlphabet[i][k];
+			}
+			getScore[j + status] = getScore[j] + score[i];
+			answer = max(answer, getScore[j + status]);
+		}
+	}
+
+}
 
 int main() {
+
 	for (int i = 1; i <= 26; i++) {
-		int num;
-		cin >> num;
-		cards[i] = num;
-	}
-
-	int M;
-	cin >> M;
-
-	vector<pair<string, int>> score;
-	while (M--) {
-		string W;
-		int S;
-		cin >> W >> S;
-
-		score.push_back(make_pair(W, S));
+		cin >> restAlphabet[0][i];
 	}
 	
+	cin >> m;
+
+	for (int i = 1; i <= m; i++) {
+		cin >> word >> score[i];
+
+		for (int j = 0; j < word.length(); j++) {
+			needAlphabet[i][word[j] - 'a' + 1] += 1;
+		}
+	}
+
+	solution();
+
+	cout << answer;
 }
